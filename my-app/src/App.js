1b36d69d976a1f907018a6c305b972c5c0b54a02
Mary <l.skyling@gmail.com>
2018-10-12 14:31:13 -0400

    passing method references

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     this.setState( {
       persons: [
-        { name: "Maximilian", age: 30},
+        { name: newName, age: 30},
         { name: "Manumanu", age: 32},
         { name: "Stephanie", age: 20},
       ]
@@ -29,10 +29,17 @@ class App extends Component {
       <div className="App">
         <h1> Hi! I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick = {this.switchNameHandler}>Switch Name</button>
-        <Person name = {this.state.persons[0].name} age= {this.state.persons[0].age} />
-        <Person name = {this.state.persons[1].name} age= {this.state.persons[1].age} > My Hobbies: Racing </Person>
-        <Person name = {this.state.persons[2].name} age= {this.state.persons[2].age} />
+        <button onClick = {() => this.switchNameHandler("yayayayay")}>Switch Name</button>
+        <Person 
+        name = {this.state.persons[0].name} 
+        age= {this.state.persons[0].age} />
+        <Person 
+        name = {this.state.persons[1].name} 
+        age= {this.state.persons[1].age} 
+        click = {this.switchNameHandler.bind(this, 'Max!!')}> My Hobbies: Racing </Person>
+        <Person 
+        name = {this.state.persons[2].name} 
+        age= {this.state.persons[2].age} />
       </div>
     );
   }
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -4,7 +4,7 @@ const person = (props) => {
     // return <p> I am a Person! An I am {Math.floor(Math.random() * 30)} years old!</p>
     return (
         <div>
-            <p> I am {props.name}! An I am {props.age} years old!</p>
+            <p onClick = {props.click}> I am {props.name}! An I am {props.age} years old!</p>
             <p>{props.children}</p>
         </div>
     )
