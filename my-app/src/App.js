edf181bfe3b61690dc37839e166750c3dca0a67e
Mary <l.skyling@gmail.com>
2018-10-12 16:32:55 -0400

    styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import './Person/Person.css';
 
 class App extends Component {
   state ={
@@ -15,7 +16,7 @@ class App extends Component {
     // console.log('Was clicked!');
     this.setState( {
       persons: [
-        { name: newName, age: 30},
+        { name: newName, age: 28},
         { name: "Manumanu", age: 32},
         { name: "Stephanie", age: 20},
       ]
@@ -24,19 +25,40 @@ class App extends Component {
     )
   }
 
+  nameChangeHandler =(event) => {
+    this.setState({
+      persons: [
+
+        { name: "Max", age: 30},
+        { name: event.target.value, age: 32},
+        { name: "Stephanie", age: 26},
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
         <h1> Hi! I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick = {() => this.switchNameHandler("yayayayay")}>Switch Name</button>
+        <button 
+        style = {style}
+        onClick = {() => this.switchNameHandler("yayayayay")}>Switch Name</button>
         <Person 
         name = {this.state.persons[0].name} 
         age= {this.state.persons[0].age} />
         <Person 
         name = {this.state.persons[1].name} 
         age= {this.state.persons[1].age} 
-        click = {this.switchNameHandler.bind(this, 'Max!!')}> My Hobbies: Racing </Person>
+        click = {this.switchNameHandler.bind(this, 'Max!!')}
+        changed={this.nameChangeHandler}> My Hobbies: Racing </Person>
         <Person 
         name = {this.state.persons[2].name} 
         age= {this.state.persons[2].age} />
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -3,9 +3,10 @@ import React from 'react';
 const person = (props) => {
     // return <p> I am a Person! An I am {Math.floor(Math.random() * 30)} years old!</p>
     return (
-        <div>
+        <div className="Person">
             <p onClick = {props.click}> I am {props.name}! An I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 }
